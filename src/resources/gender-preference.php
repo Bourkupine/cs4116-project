5c52abbe3cea48676af7b1a3cbcadba1c0b7be46
Bayan <b.nezamabad@gmail.com>
2024-03-27 10:20:30 +0000

    [BAYAN] Added list of genders and preferences

---
+++ src/resources/gender-preference.php
@@ -0,0 +1,16 @@
+
+<?php
+$var = 5;
+
+$gender_arr = array(
+    'Male',
+    'Female',
+    'Non-binary',
+);
+
+$preference_arr = array(
+    'Heterosexual',
+    'Homosexual',
+    'Bisexual',
+);
+?>
