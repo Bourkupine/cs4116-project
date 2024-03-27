7f169f33241ad69865502af3c37771a8d6b1975e
Bayan <b.nezamabad@gmail.com>
2024-03-27 19:24:47 +0000

    [BAYAN] Moved relevant scripts to bottom of register.php

--- src/app/register/register.php
+++ src/app/register/register.php
@@ -15,15 +15,6 @@ require "../../resources/languages.php";
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
     crossorigin="anonymous"
   />
-  <script
-    src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-    crossorigin="anonymous"
-  ></script>
-  <script
-    src="https://kit.fontawesome.com/3af37769b5.js"
-    crossorigin="anonymous"
-  ></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/bbbootstrap/libraries@main/choices.min.css">
   <script src="https://cdn.jsdelivr.net/gh/bbbootstrap/libraries@main/choices.min.js"></script>
@@ -147,5 +138,15 @@ require "../../resources/languages.php";
 
   </div>
 </div>
+
+<script
+  src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+  crossorigin="anonymous"
+></script>
+<script
+  src="https://kit.fontawesome.com/3af37769b5.js"
+  crossorigin="anonymous"
+></script>
 </body>
 </html>
