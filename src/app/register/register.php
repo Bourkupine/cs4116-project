e7b93fab36532cb80a78ad46247808883811a097
Bayan <b.nezamabad@gmail.com>
2024-03-27 16:11:20 +0000

    [BAYAN] Tidied up HTML and removed unnecessary code

--- src/app/register/register.php
+++ src/app/register/register.php
@@ -1,18 +1,14 @@
 <?php
-$root = realpath($_SERVER["DOCUMENT_ROOT"]);
 require "../../resources/gender-preference.php";
 require "../../resources/countries.php";
 require "../../resources/languages.php";
-
-$fluent_selected = [];
-$learning_selected = [];
 ?>
 
 <!doctype html>
 <html lang="en">
 <head>
-  <link rel="stylesheet" href="\cs4116-project\src\app\styles.css" />
-  <link rel="stylesheet" href="\cs4116-project\src\app\register\register.css" />
+  <link rel="stylesheet" href="../styles.css" />
+  <link rel="stylesheet" href="register.css" />
   <link
     href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
     rel="stylesheet"
@@ -31,6 +27,7 @@ $learning_selected = [];
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/bbbootstrap/libraries@main/choices.min.css">
   <script src="https://cdn.jsdelivr.net/gh/bbbootstrap/libraries@main/choices.min.js"></script>
+
   <title>Love Languages - Register</title>
 </head>
 
@@ -39,7 +36,7 @@ $learning_selected = [];
   <div class="row">
     <div class="col">
       <form method="post">
-        <!-- INPUT FIELDS -->
+
         <div class="basic-info">
           <div class="form-group m-2 mt-4">
             <input type="text" class="form-control" placeholder="First Name">
@@ -89,6 +86,7 @@ $learning_selected = [];
               <input type="text" class="form-control" placeholder="State/County">
             </div>
           </div>
+
           <div class="form-group ps-2 pe-2 mb-2">
             <label for="fluent-languages">I am fluent in...</label>
             <select multiple
@@ -112,10 +110,10 @@ $learning_selected = [];
 
           <script>
             $(document).ready(function(){
-              var learningLanguagesDropdown = new Choices('#learning-languages', {
+              const learningLanguagesDropdown = new Choices('#learning-languages', {
                 removeItemButton: true,
               });
-              var fluentLanguagesDropdown = new Choices('#fluent-languages', {
+              const fluentLanguagesDropdown = new Choices('#fluent-languages', {
                 removeItemButton: true,
               });
             });
@@ -124,9 +122,11 @@ $learning_selected = [];
           <div class="submit-button">
             <button type="submit" class="btn text-white">Register</button>
           </div>
+
         </div>
       </form>
     </div>
+
     <div class="col d-none d-lg-block">
       <div class="logo-image pt-5 pb-5">
         <img src="../../assets/ll-logo-gradient.png" alt="logo">
@@ -135,6 +135,7 @@ $learning_selected = [];
         <span>LOVE SPEAKS EVERY LANGUAGE</span>
       </div>
     </div>
+
   </div>
 </div>
 </body>
