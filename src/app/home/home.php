585a696a657408d992344a0811e0ed5eabf764c4
Euan Bourke <euanbourke@gmail.com>
2024-03-28 18:20:18 +0000

    home: updated buttons and text
    Moved buttons into the same div.
    Updated css to be closer to website mockups.
    
    Signed-off-by: Euan Bourke <euanbourke@gmail.com>

---
+++ src/app/home/home.php
@@ -0,0 +1,42 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Home</title>
+    <link rel="stylesheet" type="text/css" href="home.css">
+    <link rel="stylesheet" type="" href="../styles.css">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+          crossorigin="anonymous">
+</head>
+
+<body>
+
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h1 class="title slogan-text">The Dating Website for Language Lovers</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="buttons">
+            <button type="button" class="btn btn-primary">Sign Up</button>
+            <button type="button" class="btn btn-primary">Log In</button>
+        </div>
+    </div>
+
+    <div class="row">
+        <!-- img -->
+    </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous">
+</script>
+</body>
+
+</html>
