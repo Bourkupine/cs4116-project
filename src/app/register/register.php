fe804d78a9bc4b1369e8512a17ca986739f91f70
Bayan <b.nezamabad@gmail.com>
2024-03-28 17:55:13 +0000

    [BAYAN] Refactored data to register directory

--- src/app/register/register.php
+++ src/app/register/register.php
@@ -1,10 +1,10 @@
 <?php
-require "../../resources/gender-preference.php";
-require "../../resources/countries.php";
-require "../../resources/languages.php";
+require "gender-preference.php";
+require "countries.php";
+require "languages.php";
 ?>
 
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
   <link rel="stylesheet" href="../styles.css" />
