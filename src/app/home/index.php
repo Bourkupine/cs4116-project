89a4c6d934c41d417ee74a38e37740f236d44994
Euan Bourke <euanbourke@gmail.com>
2024-04-01 21:05:28 +0100

    home: added meta tags to header
    Signed-off-by: Euan Bourke <euanbourke@gmail.com>

--- src/app/home/index.php
+++ src/app/home/index.php
@@ -5,6 +5,9 @@
 
 <head>
     <title>Home</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <link rel="stylesheet" type="" href="../styles.css">
     <link rel="stylesheet" type="text/css" href="home.css">
 
