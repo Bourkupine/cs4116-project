30aeb7373acf6bf7c2b368c65c304a72962b042d
Bayan <b.nezamabad@gmail.com>
2024-04-03 10:34:29 +0100

    [BAYAN] Reformatted code and added margins

--- src/app/aboutpage/aboutpage.php
+++ src/app/aboutpage/aboutpage.php
@@ -5,95 +5,95 @@ require_once("../navbar/navbar.php");
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>About Page</title>
+  <title>About Page</title>
 
-    <link
-            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-            rel="stylesheet"
-            integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
-            crossorigin="anonymous"
-    />
-    <link rel="stylesheet" href="../styles.css"/>
-    <link rel="stylesheet" type="text/css" href="aboutpage.css"/>
+  <link
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+    rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+    crossorigin="anonymous"
+  />
+  <link rel="stylesheet" href="../styles.css" />
+  <link rel="stylesheet" type="text/css" href="aboutpage.css" />
 
 </head>
 <body>
 
 <div class="container">
-    <div class="row">
-        <div class="col-xl-6" style="text-align: center;">
-            <div class="howdoesthiswork">
-                <div class="image responsive center"><img src="../../../assets/download.png" alt="cog"></div>
-                <div class="title">
-                    <header>How does this work</header>
-                </div>
+  <div class="row">
+    <div class="col-xl-6 mt-3 mb-3" style="text-align: center;">
+      <div class="howdoesthiswork">
+        <div class="image responsive center"><img src="../../../assets/download.png" alt="cog"></div>
+        <div class="title">
+          <header>How does this work</header>
+        </div>
 
-                <div class="description"><h1>
-                    Love Languages is for the enthusiastic and single.
-                    Want to practice a language with someone new? AND go on a date!
-                    We are aspiring to re focus the date to language.
-                    By doing this we hope to take the hostility out of dating and
-                    foster a learning environment rather than a tense nervous one
-                </h1></div>
-            </div>
-        </div>
-        <div class="col-xl-6">
-            <div class="slogan slogan-text pt-3">
-                <span>LOVE SPEAKS EVERY LANGUAGE</span>
-            </div>
-        </div>
+        <div class="description"><h1>
+            Love Languages is for the enthusiastic and single.
+            Want to practice a language with someone new? AND go on a date!
+            We are aspiring to re focus the date to language.
+            By doing this we hope to take the hostility out of dating and
+            foster a learning environment rather than a tense nervous one
+          </h1></div>
+      </div>
     </div>
+    <div class="col-xl-6 mt-3 mb-3">
+      <div class="slogan slogan-text pt-3">
+        <span>LOVE SPEAKS EVERY LANGUAGE</span>
+      </div>
+    </div>
+  </div>
 
-    <div class="row">
-        <div class="col-xl-6" style="text-align: center;">
-                <div class="ourjob" >
+  <div class="row">
+    <div class="col-xl-6 mt-3 mb-3" style="text-align: center;">
+      <div class="ourjob">
 
         <div class="image responsive"><img src="../../../assets/ourjob.png" alt="cog"></div>
         <div class="title">
-            <header>Our Job</header>
+          <header>Our Job</header>
         </div>
 
         <div class="description">
-            <h1>
-                We keep only the information you give us.
-                Firstly we use our handcrafted algorithm to match you
-                with people whom are capable of speaking the
-                languages you are looking to practice
-            </h1>
+          <h1>
+            We keep only the information you give us.
+            Firstly we use our handcrafted algorithm to match you
+            with people whom are capable of speaking the
+            languages you are looking to practice
+          </h1>
         </div>
+      </div>
     </div>
+
+    <div class="col-xl-6 mt-3 mb-3">
+      <div class="yourjob" style="text-align: center;">
+        <div class="image responsive"><img src="../../../assets/yourjob.png" alt="cog"></div>
+        <div class="title">
+          <header>Your job</header>
         </div>
 
-    <div class="col-xl-6">
-        <div class="yourjob" style="text-align: center;">
-            <div class="image responsive"><img src="../../../assets/yourjob.png" alt="cog"></div>
-            <div class="title">
-                <header>Your job</header>
-            </div>
-
-            <div class="description"><h1>
-                Be respectful, Please adhere to our terms and
-                conditions and most of all have fun! This is meant to
-                be an enjoyable experience for all involved.
-            </h1></div>
-        </div>
+        <div class="description"><h1>
+            Be respectful, Please adhere to our terms and
+            conditions and most of all have fun! This is meant to
+            be an enjoyable experience for all involved.
+          </h1></div>
+      </div>
     </div>
-</div>
+  </div>
 </div>
 <script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-        crossorigin="anonymous"
+  src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+  crossorigin="anonymous"
 ></script>
 <script
-        src="https://kit.fontawesome.com/3af37769b5.js"
-        crossorigin="anonymous"
+  src="https://kit.fontawesome.com/3af37769b5.js"
+  crossorigin="anonymous"
 ></script>
 
 <script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-        crossorigin="anonymous"
+  src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+  crossorigin="anonymous"
 ></script> <!-- This is for Bootstrap JS code -->
 
 </body>
