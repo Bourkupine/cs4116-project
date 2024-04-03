a5541b23fd349687160c1f4b079eac77dd4727e3
Bayan <b.nezamabad@gmail.com>
2024-04-03 10:53:53 +0100

    [BAYAN] CSS and HTML

--- src/app/aboutpage/aboutpage.php
+++ src/app/aboutpage/aboutpage.php
@@ -1,7 +1,3 @@
-<?php
-require_once("../navbar/navbar.php");
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,26 +11,33 @@ require_once("../navbar/navbar.php");
   />
   <link rel="stylesheet" href="../styles.css" />
   <link rel="stylesheet" type="text/css" href="aboutpage.css" />
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
 
 </head>
 <body>
 
-<div class="container">
-  <div class="row">
-    <div class="col-xl-6 mt-3 mb-3" style="text-align: center;">
-      <div class="howdoesthiswork">
-        <div class="image responsive center"><img src="../../../assets/download.png" alt="cog"></div>
-        <div class="title">
-          <header>How does this work</header>
-        </div>
+<?php
+require_once("../navbar/navbar.php");
+?>
 
-        <div class="description"><h1>
-            Love Languages is for the enthusiastic and single.
-            Want to practice a language with someone new? AND go on a date!
-            We are aspiring to re focus the date to language.
-            By doing this we hope to take the hostility out of dating and
-            foster a learning environment rather than a tense nervous one
-          </h1></div>
+<div class="container">
+  <div class="row first-row">
+    <div class="col-xl-6 mt-3 mb-3" style="text-align: center;">
+      <div>
+        <img class="img-fluid" src="../../../assets/download.png" alt="cog">
+      </div>
+      <div class="title">
+        <header>How does this work</header>
+      </div>
+
+      <div class="description">
+        <p>
+          Love Languages is for the enthusiastic and single.
+          Want to practice a language with someone new? AND go on a date!
+          We are aspiring to re focus the date to language.
+          By doing this we hope to take the hostility out of dating and
+          foster a learning environment rather than a tense nervous one
+        </p>
       </div>
     </div>
     <div class="col-xl-6 mt-3 mb-3">
@@ -46,36 +49,37 @@ require_once("../navbar/navbar.php");
 
   <div class="row">
     <div class="col-xl-6 mt-3 mb-3" style="text-align: center;">
-      <div class="ourjob">
+      <div>
+        <img class="img-fluid" src="../../../assets/ourjob.png" alt="cog">
+      </div>
+      <div class="title">
+        <header>Our Job</header>
+      </div>
 
-        <div class="image responsive"><img src="../../../assets/ourjob.png" alt="cog"></div>
-        <div class="title">
-          <header>Our Job</header>
-        </div>
-
-        <div class="description">
-          <h1>
-            We keep only the information you give us.
-            Firstly we use our handcrafted algorithm to match you
-            with people whom are capable of speaking the
-            languages you are looking to practice
-          </h1>
-        </div>
+      <div class="description">
+        <p>
+          We keep only the information you give us.
+          Firstly we use our handcrafted algorithm to match you
+          with people whom are capable of speaking the
+          languages you are looking to practice
+        </p>
       </div>
     </div>
 
-    <div class="col-xl-6 mt-3 mb-3">
-      <div class="yourjob" style="text-align: center;">
-        <div class="image responsive"><img src="../../../assets/yourjob.png" alt="cog"></div>
-        <div class="title">
-          <header>Your job</header>
-        </div>
+    <div class="col-xl-6 mt-3 mb-3" style="text-align: center;">
+      <div>
+        <img class="img-fluid" src="../../../assets/yourjob.png" alt="cog">
+      </div>
+      <div class="title">
+        <header>Your job</header>
+      </div>
 
-        <div class="description"><h1>
-            Be respectful, Please adhere to our terms and
-            conditions and most of all have fun! This is meant to
-            be an enjoyable experience for all involved.
-          </h1></div>
+      <div class="description">
+        <p>
+          Be respectful, Please adhere to our terms and
+          conditions and most of all have fun! This is meant to
+          be an enjoyable experience for all involved.
+        </p>
       </div>
     </div>
   </div>
