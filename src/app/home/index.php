8a83fc1641044ae10f5f5a2ac0901405e79e6000
Euan Bourke <30806087+Bourkupine@users.noreply.github.com>
2024-04-04 20:46:51 +0100

    feat: home page (#7)
    * home: updated buttons and text
    
    Moved buttons into the same div.
    Updated css to be closer to website mockups.
    
    * home: added new button css to buttons
    
    * home: updated button params in CSS
    
    * home: renamed home.php to index.php
    
    * home: changed <button> tags to <a> tags
    
    Replaced the buttons with <a> so they can redirect.
    Currently redirecting to a placeholder spot (index.php).
    
    * home: added meta tags to header
    
    * home: added navbar
    
    * index: redirected index.php to homepage
    
    * home: made text size relative to browser size
    
    * home: added font awesome
    
    * home: added padding to row divs
    
    * home: removed img div
    
    * home: added link to log in page
    
    * home: updated CSS and move buttons into outer div
    
    * index: updated redirect from app/ to app/home/
    
    removed absolute link and replaced with relative.
    
    * home: reordered CSS links
    
    * home: updated css link
    
    * home: added redirect to register
    
    * home: removed font settings from home.css as they are in slogan-text.css
    
    * navbar: redirected logo to homepage
    
    * home: changed title
    
    * home: removed media tag from css
    
    ---------
    
    Signed-off-by: Euan Bourke <euanbourke@gmail.com>
    Signed-off-by: bourkupine <euanbourke@gmail.com>

---
+++ src/app/home/index.php
@@ -0,0 +1,45 @@
+<?php
+include "../navbar/navbar.php"
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Love Languages - Home</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+          crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="../styles.css">
+    <link rel="stylesheet" type="text/css" href="home.css">
+</head>
+
+<body>
+
+<div class="container">
+    <div class="pt-3 row">
+        <div class="col-12">
+            <h1 class="title slogan-text">The Dating Website for Language Lovers</h1>
+        </div>
+    </div>
+    <div class="buttons pt-4 row">
+        <a href="../register/index.php" class="btn ll-button" role="button">Sign Up</a>
+        <a href="../login/index.php" class="btn ll-button" role="button">Log In</a>
+    </div>
+
+</div>
+<script
+        src="https://kit.fontawesome.com/3af37769b5.js"
+        crossorigin="anonymous"
+></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous">
+</script>
+</body>
+
+</html>
