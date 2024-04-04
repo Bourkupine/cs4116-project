47a6704f8d1819fe58eae4c0b15b0baaa5742362
Bayan <b.nezamabad@gmail.com>
2024-04-04 14:06:28 +0100

    [BAYAN] Documentation for user_languages.php

--- src/app/database/user_languages.php
+++ src/app/database/user_languages.php
@@ -2,10 +2,13 @@
 
 /**
  * Inserts an array of languages into the database associated with a user_id, status, and level
-**/
-
-
-
+ * @param mysqli $db_con database connection
+ * @param int $user_id associated user's user_id
+ * @param array $language_ids ids of the user's languages
+ * @param string $status learning or speaks the language
+ * @param string $level level of fluency in the language
+ * @return bool true if query is successful
+ */
 function add_user_languages(mysqli $db_con, int $user_id, array $language_ids, string $status, string $level): bool
 {
     $query = "INSERT INTO user_languages (user_id, language_id, status, level) VALUES ";
