1e1aaeb204fd1ac7da0799df0ff11043d287239f
oOoOoOo0o <74324531+oOoOoOo0o@users.noreply.github.com>
2024-04-04 11:06:42 +0100

    feat: register screen responsive ui (#2)
    * [BAYAN] Added php plugin for prettier
    
    * [BAYAN] Added list of countries
    
    * [BAYAN] Added list of genders and preferences
    
    * [BAYAN] Added list of languages
    
    * [BAYAN] Added languages
    
    * [BAYAN] Added gradient logo
    
    * [BAYAN] Made responsive registration fields frontend
    
    * [BAYAN] Finished responsive frontend
    
    * [BAYAN] Ran prettier
    
    * [BAYAN] Fixed title saying log in instead of register
    
    * [BAYAN] Quick PR comment fixes
    
    * [BAYAN] Quick PR comment fixes
    
    * [BAYAN] Implemented better select dropdown for now
    
    * [BAYAN] Tidied up HTML and removed unnecessary code
    
    * [BAYAN] Added input sanitisation
    
    * [BAYAN] Ran prettier and cleaned code
    
    * [BAYAN] Slight bootstrap fixes
    
    * [BAYAN] Removed unneeded paragraph
    
    * [BAYAN] Moved relevant scripts to bottom of register.php
    
    * [BAYAN] Added ' and spaces allowed for last name(s)
    
    * [BAYAN] Refactored data to register directory
    
    * [BAYAN] Fixed password help and slogan css
    
    * [BAYAN] Removed redundant body bg color
    
    * [BAYAN] Removed unused fa script and moved bbbootstrap script to bottom of body
    
    * [BAYAN] Removed head and body tags
    
    * [BAYAN] Added padding to prevent overlap
    
    * [BAYAN] Removed navbar from index
    
    * [BAYAN] Moved CSS to style tags
    
    * [BAYAN] Made compatible with navbar
    
    * [BAYAN] Renamed register.php to index.php
    
    * [BAYAN] Altered CSS to work with navbar
    
    * [BAYAN] Changed to use ll-button and re-ordered stylesheets in order of precedence
    
    * [BAYAN] Removed unnecessary !important
    
    * [BAYAN] Removed unused CSS classes
    
    * [BAYAN] Added age
    
    * [BAYAN] Slight CSS changes
    
    * [BAYAN] Added connect to DB and get all languages
    
    * [BAYAN] Added DB config
    
    * [BAYAN] Populating languages array from DB
    
    * [BAYAN] Made age input smaller for Euan
    
    * [BAYAN] Removed custom border-radius as its now in global styles
    
    * [BAYAN] Moved navbar to body
    
    * [BAYAN] Added meta tag

---
+++ src/app/register/index.php
@@ -0,0 +1,181 @@
+<?php
+/**
+ * @var array $gender_arr
+ * @var array $preference_arr
+ * @var array $countries
+ */
+
+require 'gender-preference.php';
+require 'countries.php';
+require '../database/database-connect.php';
+require '../database/languages.php';
+
+global $connection;
+try {
+    $connection = connect();
+} catch (Exception $e) {
+    $code = $e->getCode();
+    $message = $e->getMessage();
+    $file = $e->getFile();
+    $line = $e->getLine();
+    echo "Exception thrown in $file on line $line: [Code $code] $message\") </script>";
+}
+$languages = get_all_languages($connection);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <link
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+    rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+    crossorigin="anonymous"
+  />
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/bbbootstrap/libraries@main/choices.min.css">
+  <link rel="stylesheet" href="../styles.css" />
+  <link rel="stylesheet" href="register.css" />
+
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
+  <title>Love Languages - Register</title>
+</head>
+
+<body>
+<?php include "../navbar/navbar.php"; ?>
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <form method="post">
+
+        <div class="basic-info">
+          <div class="form-group m-2 mt-4">
+            <input type="text" class="form-control" aria-describedby="" placeholder="First Name"
+                   required minlength="2" maxlength="32" pattern="^[A-Za-z]+$">
+          </div>
+          <div class="form-group m-2">
+            <input type="text" class="form-control" placeholder="Last Name"
+                   required minlength="2" maxlength="32" pattern="^[A-Za-z ']+$">
+          </div>
+          <div class="form-group m-2">
+            <input type="email" class="form-control" placeholder="Email"
+                   required maxlength="64" pattern="^[\w\.-]+@([\w-]+\.)+[\w-]{2,5}$">
+          </div>
+          <div class="form-group m-2">
+            <input type="password" class="form-control" placeholder="Password" aria-describedby="passwordHelp"
+                   required minlength="8" maxlength="32" pattern="(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}">
+            <small id="passwordHelp" class="form-text text-muted">
+              Passwords need 8 or more characters with a number, uppercase, and lowercase character
+            </small>
+          </div>
+          <div class="form-group m-2">
+            <input type="password" class="form-control" placeholder="Confirm Password"
+                   required minlength="8" maxlength="32" pattern="(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}">
+          </div>
+
+          <div class="row ps-2 pe-2 mb-2">
+            <div class="col">
+              <select class="form-control" required>
+                <option value="" disabled selected>Gender</option>
+                  <?php foreach ($gender_arr as $gender) {
+                      echo "<option value=\"$gender\">$gender</option>";
+                  } ?>
+              </select>
+            </div>
+            <div class="col">
+              <select class="form-control col" required>
+                <option value="" disabled selected>I am looking for...</option>
+                  <?php foreach ($preference_arr as $pref) {
+                      echo "<option value=\"$pref\">$pref</option>";
+                  } ?>
+              </select>
+            </div>
+          </div>
+
+          <div class="row ps-2 pe-2 mb-2">
+            <div class="form-group col">
+              <select class="form-control col" required>
+                <option value="" disabled selected>Country</option>
+                  <?php foreach ($countries as $country) {
+                      echo "<option value=\"$country\">$country</option>";
+                  } ?>
+              </select>
+            </div>
+            <div class="form-group col">
+              <input type="text" class="form-control" placeholder="State/County" required
+                     minlength="2" maxlength="32" pattern="[A-Za-z0-9,'-]+">
+            </div>
+          </div>
+
+          <div class="form-group m-2">
+            <input type="number" class="form-control w-25" placeholder="Age"
+                   required min="18" max="120" pattern="[0-9]{2,5}">
+          </div>
+
+          <div class="form-group ps-2 pe-2 mb-2">
+            <label for="fluent-languages">I am fluent in...</label>
+            <select multiple
+                    class="language-select form-control"
+                    id="fluent-languages">
+                <?php foreach ($languages as $language) {
+                    echo "<option value=\"$language\">$language</option>";
+                } ?>
+            </select>
+          </div>
+          <div class="form-group ps-2 pe-2 mb-2">
+            <label for="learning-languages">I am learning...</label>
+            <select multiple
+                    class="language-select form-control"
+                    id="learning-languages">
+                <?php foreach ($languages as $language) {
+                    echo "<option value=\"$language\">$language</option>";
+                } ?>
+            </select>
+          </div>
+
+          <script>
+            $(document).ready(function() {
+              const learningLanguagesDropdown = new Choices("#learning-languages", {
+                removeItemButton: true
+              });
+              const fluentLanguagesDropdown = new Choices("#fluent-languages", {
+                removeItemButton: true
+              });
+            });
+          </script>
+
+          <div class="submit-button">
+            <button type="submit" class="btn text-white ll-button">Register</button>
+          </div>
+
+        </div>
+      </form>
+    </div>
+
+    <div class="col d-none d-lg-block">
+      <div class="logo-image pt-3 pb-4">
+        <img src="../../assets/ll-logo-gradient.png" alt="logo">
+      </div>
+      <div class="slogan slogan-text pt-2">
+        <span>LOVE SPEAKS EVERY LANGUAGE</span>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+<script
+  src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+  crossorigin="anonymous"
+></script>
+<script
+  src="https://cdn.jsdelivr.net/gh/bbbootstrap/libraries@main/choices.min.js"
+  crossorigin="anonymous"
+></script>
+<script
+  src="https://kit.fontawesome.com/3af37769b5.js"
+  crossorigin="anonymous"
+></script>
+</body>
+</html>
