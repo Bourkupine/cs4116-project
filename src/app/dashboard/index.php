9752f3ffc2dae5ecb86bd7063b66c1d56d7017a0
Bayan <b.nezamabad@gmail.com>
2024-04-05 07:29:04 +0100

    [BAYAN] Created temporary login-only screens

---
+++ src/app/dashboard/index.php
@@ -0,0 +1,16 @@
+<head>
+    <?php require_once "../templates/header.php"; ?>
+  <title>Love Languages - Dashboard</title>
+</head>
+
+<body>
+<?php require_once "../navbar/navbar.php"; ?>
+
+<?php
+if (!isset($_SESSION["logged-in"])) {
+    header("Location: ../home");
+} ?>
+<h1 class="slogan-text">COMING SOON!</h1>
+
+<?php require_once "../templates/footer.php"; ?>
+</body>
