7a6cef3fed863a6c72961322e4ad9e8d846ee501
Euan Bourke <euanbourke@gmail.com>
2024-04-07 20:17:54 +0100

    admin: updated layout moving buttons
    Also created function file
    
    Signed-off-by: Euan Bourke <euanbourke@gmail.com>

--- src/app/admin/index.php
+++ src/app/admin/index.php
@@ -21,22 +21,25 @@ require_once("../navbar/navbar.php");
     </div>
     <div class="list-group">
         <div class="card mb-3 bg-danger-subtle">
-            <div class="row align-items-center">
-                <div class="col-2 pics">
-                    <img class="img-fluid" src="../../assets/ll-logo.png" alt="...">
+            <form method="post" action="resolve_report.php">
+                <div class="row align-items-center">
+                    <div class="col-2 pics">
+                        <img class="img-fluid" src="../../assets/ll-logo.png" alt="...">
+                    </div>
+                    <div class="col-6 card-body g-0">
+                        <input readonly class="card-text form-control-plaintext my-0"><b>Report ID: </b>123</input>
+                        <input readonly class="card-text form-control-plaintext my-0"><b>User ID: </b>12345</input>
+                        <input readonly class="card-text form-control-plaintext my-0"><b>Reason: </b>Sent me nudes, pls ban immedientlylel</input>
+                        <input readonly class="card-text form-control-plaintext my-0"><b>Reported by: </b>127683</input>
+                    </div>
                 </div>
-                <div class="col-6 card-body">
-                    <p class="card-text my-0"><b>User ID</b>: 12345</p>
-                    <p class="card-text my-0"><b>Reason</b>: Sent me nudes, pls ban immedientlylel
-                    <p class="card-text m-0"><b>Reported by</b>: 127683</p>
+                <div class="row text-center">
+                    <div class="col-5"><a href="#" class="btn buttons bg-danger ll-button my-2 me-4" role="button">Ban</a></div>
+                    <div class="col-5"><a href="#" class="btn buttons bg-success ll-button my-2 me-4" role="button">Ignore</a>
+                    </div>
                 </div>
-            </div>
-            <div class="row text-center">
-                <div class="col-5"><a href="#" class="btn buttons bg-danger ll-button my-2 me-4" role="button">Ban</a></div>
-                <div class="col-5"><a href="#" class="btn buttons bg-success ll-button my-2 me-4" role="button">Ignore</a></div>
-            </div>
+            </form>
         </div>
-
     </div>
 </div>
 
