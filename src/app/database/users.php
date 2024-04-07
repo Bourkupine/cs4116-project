d8db87f8be2e2a711472c66565e5f9f0b30449ae
Bayan <b.nezamabad@gmail.com>
2024-04-07 20:28:28 +0100

    [BAYAN] Fixed login where email doesn't exist

--- src/app/database/users.php
+++ src/app/database/users.php
@@ -62,9 +62,9 @@ function delete_user_by_user_id(mysqli $db_con, int $user_id): bool
  * Gets a user's password hash from the users table using a given email
  * @param mysqli $db_con database connection
  * @param string $email user's email
- * @return string the user's password hash
+ * @return ?string the user's password hash
  */
-function get_password_by_email(mysqli $db_con, string $email): string {
+function get_password_by_email(mysqli $db_con, string $email): ?string {
     $stmt = $db_con->prepare("SELECT password FROM users WHERE email = ?");
     $stmt->bind_param("s", $email);
     $stmt->bind_result($password);
