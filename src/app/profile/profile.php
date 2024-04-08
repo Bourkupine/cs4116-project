540f652a537d7334cf386754544eecd1028a96b2
carthy27 <03thomasmc@gmail.com>
2024-04-08 17:57:35 +0100

    log out button

--- src/app/profile/profile.php
+++ src/app/profile/profile.php
@@ -1,1 +1,21 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Profile Page</title>
+    <link rel="stylesheet" href="./profile.css">
+
+</head>
+<body>
+<main>
+    <h1></h1>
+
+    <form method="post" class="ms-5">
+        <button name="submit" type="submit" class="btn btn-danger">Log out</button>
+    </form>
+</main>
+
+</body>
+</html>
