b85b2c10e758e1a60f8220f2b5c6ba5f5484a566
carthy27 <03thomasmc@gmail.com>
2024-04-09 16:12:53 +0100

    editing firstname

--- src/app/database/user_interests.php
+++ src/app/database/user_interests.php
@@ -19,7 +19,17 @@ function get_user_interests_by_user_id(mysqli $db_con, int $user_id): ?array {
     return $user_interest_ids;
 }
 
-function update_user_interests_by_id(mysqli $dbcon, int $user_id): ?array
+function update_user_interests_by_id(mysqli $dbcon, int $user_id, array $user_interests): ?array
 {
- return 0;
+
+    $current_user_interests[]=get_user_interests_by_user_id($dbcon,$user_id);
+    for ($i = 0; $i < sizeof($current_user_interests); $i++) {
+        if($current_user_interests[$i] != $user_interests[$i]){
+            $current_user_interests[$i] = $user_interests[$i];
+        }
+ }
+    $stmt = $dbcon->prepare("UPDATE user_interests SET interest_id WHERE user_id =?");
+    $stmt->bind_param("i", $interest_id);
+    $stmt->bind_result($user_interest_id);
+    $stmt->execute();
 }
