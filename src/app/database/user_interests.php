7f031e678f16e0211cd076552cb5511addb2a492
Bayan <b.nezamabad@gmail.com>
2024-04-09 13:39:10 +0100

    [BAYAN] Fixed SQL query

--- src/app/database/user_interests.php
+++ src/app/database/user_interests.php
@@ -7,8 +7,14 @@
  * @return array | null interest ids
  */
 function get_user_interests_by_user_id(mysqli $db_con, int $user_id): ?array {
+    $user_interest_ids = array();
     $stmt = $db_con->prepare("SELECT interest_id FROM user_interests WHERE user_id = ?");
     $stmt->bind_param("s", $user_id);
+    $stmt->bind_result($user_interest_id);
     $stmt->execute();
-    return ($stmt->get_result()->fetch_assoc());
+
+    while ($stmt->fetch()) {
+        $user_interest_ids[] = $user_interest_id;
+    }
+    return $user_interest_ids;
 }
