c9d58c11c190317c6fd093cb55b4382e594cc14e
bourkupine <euanbourke@gmail.com>
2024-04-13 16:58:16 +0100

    admin: back end functions written
    Signed-off-by: bourkupine <euanbourke@gmail.com>

--- src/app/admin/index.php
+++ src/app/admin/index.php
@@ -1,3 +1,24 @@
+<?php
+
+require_once "../database/database_connect.php";
+
+require_once "resolve_report.php";
+
+
+try {
+    $connection = connect();
+    $report_list = get_reports($connection);
+} catch (Exception $e) {
+    $code = $e->getCode();
+    $message = $e->getMessage();
+    $file = $e->getFile();
+    $line = $e->getLine();
+    echo "<script> console.log(\"Exception thrown in $file on line $line: [Code $code] $message\"); </script>";
+}
+
+print_r($report_list);
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,9 +34,13 @@
 
 <?php
 require_once("../navbar/navbar.php");
+
+
+
+
 ?>
 
-<div class="container">
+<div class="container-fluid">
     <div class="row heading">
         <h2>Admin Dashboard</h2>
     </div>
@@ -23,19 +48,29 @@ require_once("../navbar/navbar.php");
         <div class="card mb-3 bg-danger-subtle">
             <form method="post" action="resolve_report.php">
                 <div class="row align-items-center">
-                    <div class="col-2 pics">
+                    <div class="col-3 pics">
                         <img class="img-fluid" src="../../assets/ll-logo.png" alt="...">
                     </div>
-                    <div class="col-6 card-body g-0">
-                        <input readonly class="card-text form-control-plaintext my-0"><b>Report ID: </b>123</input>
-                        <input readonly class="card-text form-control-plaintext my-0"><b>User ID: </b>12345</input>
-                        <input readonly class="card-text form-control-plaintext my-0"><b>Reason: </b>Sent me nudes, pls ban immedientlylel</input>
-                        <input readonly class="card-text form-control-plaintext my-0"><b>Reported by: </b>127683</input>
+                    <div class="col-9">
+                        <div class="row">
+                            <div class="col">
+                                <p class="card-text">Report ID: 213</p>
+                                <p class="card-text">Reason: asda asdasd asd asdd asdasd asd asd asd asd asd asd asd asd </p>
+                                <p class="card-text">Reported By: 3123</p>
+                            </div>
+                            <div class="col">
+                                <p class="card-text">User ID: 4312</p>
+                                <p class="card-text">Name: Blake Ryan</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div class="row text-center">
-                    <div class="col-5"><a href="#" class="btn buttons bg-danger ll-button my-2 me-4" role="button">Ban</a></div>
-                    <div class="col-5"><a href="#" class="btn buttons bg-success ll-button my-2 me-4" role="button">Ignore</a>
+                    <div class="col-5">
+                        <button name="ban" type="submit" class="btn buttons bg-danger ll-button my-2 me-4" role="button">Ban</button>
+                    </div>
+                    <div class="col-5">
+                        <button name="ignore" type="submit" class="btn buttons bg-success ll-button my-2 me-4" role="button">Ignore</button>
                     </div>
                 </div>
             </form>
--- src/app/admin/resolve_report.php
+++ src/app/admin/resolve_report.php
@@ -1,2 +1,36 @@
 <?php
-require "../database/database_connect.php";
+
+function get_reports(mysqli $db_con): array
+{
+    $sql = "SELECT report_id, reporter_id, reportee_id, reason FROM reports WHERE resolved = 0";
+
+    $stmt = $db_con->prepare($sql);
+    $stmt->bind_result($report_id, $reporter_id, $reportee_id, $reason);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    $reports_arr = array();
+
+    while ($report = $result->fetch_assoc()) {
+        $reports_arr[] = $report;
+    }
+
+    return $reports_arr;
+}
+
+function get_users_report_info(mysqli $db_con, int $user_id): array {
+
+    $sql = "SELECT first_name, surname, profile_pic FROM profiles where user_id = ?";
+
+    $stmt = $db_con->prepare($sql);
+    $stmt->bind_param("i", $user_id);
+    $stmt->bind_result($first_name, $surname, $profile_pic);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    //run check for pfp and use default if none exists
+    $user_info = array($result['first_name'], $result['surname'], $result['profile_pic']);
+
+    return $user_info;
+
+}
