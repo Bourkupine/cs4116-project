e50cd53c4ac377605cb0210e015e937c0f6e64c8
Bayan <b.nezamabad@gmail.com>
2024-04-25 12:05:30 +0100

    [BAYAN] Updated look of about

--- src/app/about/index.php
+++ src/app/about/index.php
@@ -15,7 +15,7 @@ require_once("../navbar/navbar.php");
 
 <div class="container">
     <div class="row first-row">
-        <div class="col-xl-6 mt-3 mb-3 pe-4" style="text-align: center;">
+        <div class="col-12 col-xl mt-3 mb-3 me-4 pt-3 about-card" style="text-align: center;">
             <div>
                 <img class="img-fluid" src="../../assets/cog.png" alt="cog">
             </div>
@@ -33,7 +33,7 @@ require_once("../navbar/navbar.php");
                 </p>
             </div>
         </div>
-        <div class="col-xl-6 mt-3 mb-3 ps-4">
+        <div class="col-12 col-xl mt-3 mb-3 ps-4">
             <div class="slogan slogan-text pt-3">
                 <span>LOVE SPEAKS EVERY LANGUAGE</span>
             </div>
@@ -41,7 +41,7 @@ require_once("../navbar/navbar.php");
     </div>
 
     <div class="row">
-        <div class="col-xl-6 mt-3 mb-3 pe-4" style="text-align: center;">
+        <div class="col-12 col-xl mt-3 mb-3 me-4 pt-3 about-card" style="text-align: center;">
             <div>
                 <img class="img-fluid" src="../../assets/server.png" alt="cog">
             </div>
@@ -60,7 +60,7 @@ require_once("../navbar/navbar.php");
             </div>
         </div>
 
-        <div class="col-xl-6 mt-3 mb-3 ps-4" style="text-align: center;">
+        <div class="col-12 col-xl mt-3 mb-3 pt-3 me-4 about-card" style="text-align: center;">
             <div>
                 <img class="img-fluid" src="../../assets/mindset.png" alt="cog">
             </div>
